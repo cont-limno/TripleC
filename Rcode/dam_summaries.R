826c93621270b796d27cdeae8d02ec14dc3939d5
kingka21 <kingka21@msu.edu>
2021-06-21 12:57:26 -0400

    updated data files for publication

--- Rcode/dam_summaries.R
+++ Rcode/dam_summaries.R
@@ -6,14 +6,13 @@
 
 #### R libraries ####
 library(dplyr)
-library(ggplot2)
 
 #### Input data ####
 #LAGOS_US_NETWORKS_v1 all lakes (King et al. 2021)
-lagos_networks_all <- read.csv("Data/Networks/nets_networkmetrics_medres.csv") 
+lagos_networks_all <- read.csv("Data/Networks/nets_networkmetrics_medres_dams.csv") 
 #just network data
-lagos_networks<-lagos_networks_all[!duplicated(paste(lagos_networks_all$net_id)),] %>% 
-  dplyr::select(lagoslakeid, net_id, net_lakes_n, net_dams_n)
+lagos_networks<-read.csv('Data/Networks/network_scale_metrics.csv') %>% 
+  dplyr::select(net_id, net_lakes_n, net_dams_n)
 #hub lakes 
 hub_lakes<- read.csv("Data/Networks/VIP_lakes.csv")
 
@@ -22,7 +21,7 @@ hub_lakes<- read.csv("Data/Networks/VIP_lakes.csv")
 #### dams on hub lakes #### 
 hub_char<-left_join(hub_lakes, lagos_networks_all)
 
-#new columns for directly up or down dam 
+#new columns for directly up or down dam from a hub lake 
 hub_char <- hub_char %>%
   mutate(down_dam = case_when(is.na(lake_nets_nearestdamdown_km) ~ "N",   #if ~ then 
                             TRUE ~"Y"
@@ -34,13 +33,13 @@ hub_char <- hub_char %>%
 # summary of hub_char table to see how many hub lakes have dams upstream or downstream or on the lake
 hub_char$down_dam<-as.factor(hub_char$down_dam)
 hub_char$up_dam<-as.factor(hub_char$up_dam)
-summary(hub_char)
+summary(hub_char) #total dams upstream and downstream and dams on lakes 
 
 
 #number of dams on the network of the hub lake 
 hist(hub_char$net_dams_n) #32 hub lakes have no dams in their entire network (27 unique networks)
 
-hub_nets<-hub_char[!duplicated(paste(hub_char$net_id)),] #hub networks 246 networks with hub lakes 
+hub_nets<-hub_char[!duplicated(paste(hub_char$net_id)),] #hub networks: 246 networks with hub lakes 
 
 #### dams on networks with conny scores #### 
 nets_with_conn<-filter(lagos_networks, net_lakes_n > 4)
