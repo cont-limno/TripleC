874b5ebc361a7b316164fb8fe5e995ca021372d8
immccull95 <immccull@gmail.com>
2022-02-14 10:55:37 -0500

    dam it

--- Rcode/dam_summaries.R
+++ Rcode/dam_summaries.R
@@ -1,6 +1,6 @@
 ######################## Dam data summaries ###########################################
 #created: 05-NOV-2020
-#updated: 21-JN-2021
+#updated: 12-FEB-2022
 # Author: Katelyn King
 ################################################################################################
 
@@ -14,7 +14,7 @@ lagos_networks_all <- read.csv("Data/Networks/nets_networkmetrics_medres_dams.cs
 lagos_networks<-read.csv('Data/Networks/network_scale_metrics.csv') %>% 
   dplyr::select(net_id, net_lakes_n, net_dams_n)
 #hub lakes 
-hub_lakes<- read.csv("Data/Networks/VIP_lakes.csv")
+hub_lakes<- read.csv("Data/Networks/VIP_lakes.csv")#non MS river hub lakes
 
 ################### Main program ######################
 
@@ -33,8 +33,10 @@ hub_char <- hub_char %>%
 # summary of hub_char table to see how many hub lakes have dams upstream or downstream or on the lake
 hub_char$down_dam<-as.factor(hub_char$down_dam)
 hub_char$up_dam<-as.factor(hub_char$up_dam)
+hub_char$lake_totalupdowndams <- hub_char$lake_nets_totaldamup_n + hub_char$lake_nets_totaldamdown_n
 summary(hub_char) #total dams upstream and downstream and dams on lakes 
-
+hist(hub_char$lake_totalupdowndams, main='Number of dams associated with given hub lake',
+     breaks=seq(0,305,5))
 
 #number of dams on the network of the hub lake 
 hist(hub_char$net_dams_n) #32 hub lakes have no dams in their entire network (27 unique networks)
